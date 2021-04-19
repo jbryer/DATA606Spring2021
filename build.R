19838750541c137ac49552d6e7a92364ab1b0ea9
jbryer <jason@bryer.org>
2021-04-19 12:00:55 -0400

    Added PDFs of slides.

--- build.R
+++ build.R
@@ -1,4 +1,10 @@
+# usethis::gh_token_help()
+# remotes::install_github("jhelvy/xaringanBuilder")
+# credentials::set_github_pat()
+
 # Builds the course website.
+
+library(xaringanBuilder)
 
 # First copy the Slides and supplemental materials to the docs/folder
 tocopy <- c(list.files('Slides', pattern = '.html'),
@@ -11,14 +17,23 @@ for(i in tocopy) {
 	success <- FALSE
 	if(!file_test("-f", from)) { # Directory
 		dir.create(to, recursive = TRUE)
-		success <- file.copy(from, 'docs/slides/', recursive = TRUE)
+		success <- file.copy(from, 'docs/slides/', recursive = TRUE, overwrite = TRUE)
 	} else { # File
 		success <- file.copy(from, to, overwrite = TRUE)
-
 	}
 	if(!success) {
 		cat(paste0('ERROR: ', i, ' did not copy!\n'))
 	}
+
+	wd <- setwd('Slides/')
+	tryCatch({
+		build_pdf(i,
+				  complex_slides = TRUE,
+				  partial_slides = FALSE)
+	}, error = function(e) {
+		cat(paste0('Error generating PDF from ', from))
+		print(e)
+	}, finally = { setwd(wd) })
 }
 wd <- setwd('website')
 blogdown::build_site(build_rmd = TRUE)
